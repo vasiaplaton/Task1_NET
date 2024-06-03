87a61eeaeebdbea5e318c02b0219509319db9802
Vasia Platon <vasia.platon@gmail.com>
2024-06-03 11:29:59 +0300

    added_all

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Task1.models.Set<string> set;
+        public MainWindow()
+        {
+            InitializeComponent();
+            set = new Task1.models.Set<string>();
+            UpdateSetContent();
+        }
+
+        private void AddElement_Click(object sender, RoutedEventArgs e)
+        {
+            string element = txtElement.Text;
+            if (!string.IsNullOrWhiteSpace(element))
+            {
+                set.Add(element);
+                UpdateSetContent();
+            }
+        }
+
+        private void RemoveElement_Click(object sender, RoutedEventArgs e)
+        {
+            string element = txtElement.Text;
+            if (!string.IsNullOrWhiteSpace(element))
+            {
+                set.Remove(element);
+                UpdateSetContent();
+            }
+        }
+
+        private void ClearSet_Click(object sender, RoutedEventArgs e)
+        {
+            set.Clear();
+            UpdateSetContent();
+        }
+
+        private void GetArray_Click(object sender, RoutedEventArgs e)
+        {
+            var array = set.ToArray();
+            txtArray.Text = "Массив: {" + string.Join(", ", array) + "}";
+        }
+
+        private void CheckElement_Click(object sender, RoutedEventArgs e)
+        {
+            string element = txtCheckElement.Text;
+            if (!string.IsNullOrWhiteSpace(element))
+            {
+                bool contains = set.Contains(element);
+                txtContains.Text = $"Содержит элемент: {contains}";
+            }
+        }
+
+        private void UpdateSetContent()
+        {
+            txtSetContent.Text = set.ToString();
+            txtCount.Text = $"Всего элементов: {set.Count}";
+            txtIsEmpty.Text = $"Пустой: {set.IsEmpty}";
+            txtArray.Text = "Массив:";
+            txtContains.Text = $"Содержит элемент:";
+        }
+    }
+}
---
+++ models/MySet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.models
+{
+    public class Set<T>
+    {
+        private List<T> items;
+
+        public Set()
+        {
+            items = new List<T>();
+        }
+
+        public int Count => items.Count;
+        public bool IsEmpty => Count == 0;
+
+        public void Add(T item)
+        {
+            if (!Contains(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var i in items)
+            {
+                if (EqualityComparer<T>.Default.Equals(i, item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public T[] ToArray()
+        {
+            return items.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return "{" + string.Join(", ", items) + "}";
+        }
+    }
+
+}
